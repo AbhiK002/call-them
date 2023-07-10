2e0816cfd590ec1c8dfc99112e1cbd0c7d643d02
Abhineet Kelley <abhineet.kelley@gmail.com>
2023-07-10 23:23:56 +0530

    browser router basename added

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='https://abhik002.github.io/call-them'>
     <App />
   </BrowserRouter>
 )
